9f6eb36fa2c1649bd152f6b465b11538f1da9fc2
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-09-20 22:08:38 +0700

    Router: Fix eslint
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import authentication from "../middlewares/authentication";
+import {Router} from 'express';
+import authentication from '../middlewares/authentication';
 
 export const routers = Router();
 
@@ -14,8 +14,12 @@ routers.post('/logout', () => {});
  * User Routes
  */
 routers.use(['/users', '/user'], [authentication()]);
-routers.get('/users', (req, res) => {res.send("ok")});
-routers.get('/user/:id', (req, res) => {res.send("ok2")});
+routers.get('/users', (req, res) => {
+  res.send('ok');
+});
+routers.get('/user/:id', (req, res) => {
+  res.send('ok2');
+});
 routers.post('/user/store', () => {});
 routers.put('/user/update', () => {});
 routers.delete('/user/delete/:id', () => {});
@@ -23,9 +27,7 @@ routers.delete('/user/delete/:id', () => {});
 /**
  * Visual Novel Routes
  */
-routers.use(['/visualnovels', '/visualnovel'], [
-    authentication()
-]);
+routers.use(['/visualnovels', '/visualnovel'], [authentication()]);
 routers.get('/visualnovels', () => {});
 routers.get('/visualnovel/:id', () => {});
 routers.post('/visualnovel/store', () => {});
@@ -35,12 +37,9 @@ routers.delete('/visualnovel/delete/:id', () => {});
 /**
  * Birthday Routes
  */
-routers.use(['/birthdays', '/birthdays'], [
-    authentication()
-]);
+routers.use(['/birthdays', '/birthdays'], [authentication()]);
 routers.get('/birthdays', () => {});
 routers.get('/birthday/:id', () => {});
 routers.post('/birthday/store', () => {});
 routers.put('/birthday/update', () => {});
 routers.delete('/birthday/delete/:id', () => {});
-
