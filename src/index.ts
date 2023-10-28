1ca611e9c04af182d1ce729d6f9d66a583f35fa2
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-10-28 21:16:40 +0700

    Server: Tidy up at index.ts and remove unused package & files
    Tidy up code at index.ts, then remove fanTL feature (this feature
    will be release later).
    
    Then remove mongoose-fuzzy-searching package.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
-import cors from "cors";
-import { config } from "dotenv";
-import bodyParser from "body-parser";
-import express, { Express } from "express";
-import { routers } from "./routes";
-import { db } from "./configs/database";
+import cors from 'cors';
+import {config} from 'dotenv';
+import express, {Express} from 'express';
+import {routers} from './routes';
+import {connect, set} from 'mongoose';
+import bodyParser from 'body-parser';
 
-config();
+async function connectToDatabase() {
+  try {
+    set('strictQuery', false);
+    await connect(process.env.MONGODB_URI!);
+    console.info('Database Connected');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const app: Express = express();
-const port = process.env.PORT;
-db();
+function intializeServer() {
+  // Declare express server and port
+  const app: Express = express();
+  const port = process.env.PORT;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  // Implement CORS, body parser and routers
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use('/api/v1', routers);
 
-app.use('/api/v1', routers);
-
-app.listen(port, () => {
-    console.info(`ATRI API Version ${process.env.VERSION}`);
+  // Start server
+  app.listen(port, () => {
     console.info(`Server is running on port ${port}`);
-});
+    console.info(`and live on ${new Date().toUTCString()}`);
+  });
+}
+
+async function main() {
+  config();
+  console.info(`ATRI API Version ${process.env.VERSION}`);
+  console.info('\nConnecting to database and initialize server\n');
+  await connectToDatabase();
+  intializeServer();
+}
+
+main();
